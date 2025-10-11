9be28ced91a6ed3670c361156467c14abb73921e
Natalie Gokkaya <natkaya@yahoo.com>
2025-10-11 09:57:14 +0100

    Secret key added and referenced

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_xoXHUwp97Msm@ep-steep-water-agcxjz7q.c-2.eu-central-1.aws.neon.tech/echo_buck_rebel_822952")
+
+os.environ.setdefault("SECRET_KEY", "spaghetti754)£%%8VFElo23sfgh")
