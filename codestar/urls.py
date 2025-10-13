fbf8f23320b61ec8209247996f1abae86428b23c
Natalie Gokkaya <natkaya@yahoo.com>
2025-10-13 12:15:37 +0100

    Create Html to view post

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,21 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import home_view
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', home_view, name='landing-page'),
+    path('', include('blog.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', home_view, name='landing-page'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
