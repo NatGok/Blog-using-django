865b2cf065c948a06f09b5b43d9285517abb8623
Natalie Gokkaya <natkaya@yahoo.com>
2025-10-18 14:47:06 +0100

    fix syntex error

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages  
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -155,8 +156,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-MESSAGE.TAGS = {
-    messages.ERROR: 'danger',
+MESSAGE_TAGS = {
+    messages.ERROR: 'alert-danger',
+    messages.SUCCESS: 'alert-success',
 }
 
 # Static files (CSS, JavaScript, Images)
